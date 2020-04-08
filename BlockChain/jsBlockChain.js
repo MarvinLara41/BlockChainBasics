e64e094f064ee18dbf385b065e310358792295f3
marvin lara <laramarvin105@gmail.com>
2020-04-08 16:51:06 -0400

    added a simple proof of work concept

--- BlockChain/jsBlockChain.js
+++ BlockChain/jsBlockChain.js
@@ -8,6 +8,7 @@ class Block {
 		this.previousHash = previousHash;
 		this.hash = '';
 		this.hash = this.calculateHash();
+		this.nonce = 0;
 	}
 
 	calculateHash() {
@@ -16,8 +17,19 @@ class Block {
 			this.index +
 				this.timestamp +
 				this.previousHash +
-				JSON.stringify(this.data)
+				JSON.stringify(this.data) +
+				this.nonce
 		).toString();
+	}
+
+	mineNewBlock(difficulty) {
+		while (
+			this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')
+		) {
+			this.nonce++;
+			this.hash = this.calculateHash();
+		}
+		console.log('a new block was mined ' + this.hash);
 	}
 }
 
@@ -25,6 +37,7 @@ class BlockChain {
 	constructor() {
 		//the first variable of the array will be the genesis block, created manually
 		this.chain = [this.createGenesisBlock()];
+		this.difficulty = 4;
 	}
 
 	createGenesisBlock() {
@@ -39,8 +52,24 @@ class BlockChain {
 
 	addBlock(newBlock) {
 		newBlock.previousHash = this.getLastestBlock().hash;
-		newBlock.hash = newBlock.calculateHash();
+		newBlock.mineNewBlock(this.difficulty);
 		this.chain.push(newBlock);
+	}
+
+	checkBlockChainValid() {
+		for (let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i - 1];
+
+			if (currentBlock.hash !== currentBlock.calculateHash()) {
+				return false;
+			}
+
+			if (currentBlock.previousHash !== previousBlock.hash) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
 
@@ -52,7 +81,9 @@ let block2 = new Block(2, '4/6/2020', { mybalance: 50 });
 let myBlockChain = new BlockChain();
 
 //adding the new blocks to the block chain
+console.log('The first block creation');
 myBlockChain.addBlock(block);
+console.log('The second block creation');
 myBlockChain.addBlock(block2);
 
 console.log(JSON.stringify(myBlockChain, null, 4));
